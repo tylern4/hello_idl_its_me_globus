5376b900657433db30f0f0ecacf5e0fc17ee4cdb
Nicholas Tyler <tylern@lbl.gov>
2024-05-31 09:43:02 -0700

    Updating for locations on Perlmutter

--- register_hello.py
+++ register_hello.py
@@ -6,19 +6,28 @@ gc = Client()
 ENV_PATH = "./hello.env"
 load_dotenv(dotenv_path=ENV_PATH)
 
+
 def hello_python_world():
     import subprocess
-    command = f"python /fusion/projects/results/ionorbgpu/workflow_files/ionorbgpu/v2/tools/hello.py"
-    res = subprocess.run(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = [
+        "/global/common/software/nersc/pe/conda-envs/24.1.0/python-3.11/nersc-python/bin/python",
+        "/dvs_ro/cfs/cdirs/m3792/tylern/DIII-D/hello_idl_its_me_globus/hello.py"
+    ]
+    res = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return res
+
 
 def hello_idl_world():
     import subprocess
-    command = f"idl /fusion/projects/results/ionorbgpu/workflow_files/ionorbgpu/v2/tools/hello.pro"
-    res = subprocess.run(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = [
+        "/global/common/software/nersc/pm-2022q2/sw/idl/idl89/idl/bin/idl",
+        "/dvs_ro/cfs/cdirs/m3792/tylern/DIII-D/hello_idl_its_me_globus/hello.pro"
+    ]
+    res = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return res
 
-def register_functions(functions={"HELLO_PYTHON":hello_python_world, 
+
+def register_functions(functions={"HELLO_PYTHON": hello_python_world,
                                   "HELLO_IDL": hello_idl_world}):
     function_ids = {}
     for envvar in functions:
@@ -30,6 +39,6 @@ def register_functions(functions={"HELLO_PYTHON":hello_python_world,
 
     return function_ids
 
+
 if __name__ == '__main__':
-    
     register_functions()
